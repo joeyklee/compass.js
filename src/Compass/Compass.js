59dfedc9cfcd046e08b70df2179b363673f57c7c
joeyklee <joeyklee@nyu.edu>
2020-04-23 15:16:42 -0400

    adds correct event scope

--- src/Compass/Compass.js
+++ src/Compass/Compass.js
@@ -15,8 +15,8 @@ class Compass {
 
     this.debug = false;
 
-    this.permissionTrigger = document.createElement('button');
-    this.permissionTrigger.textContent = 'allow'
+    this.permissionTrigger = document.createElement("button");
+    this.permissionTrigger.textContent = "allow";
 
     this.ready = this.callCallback(this.init(), callback);
   }
@@ -26,40 +26,44 @@ class Compass {
    * @function
    */
   async init() {
-    try{
+    try {
       await this.watchPosition();
 
-      this.permissionTrigger.addEventListener('click', this.allowOrientationPermissions)
+      this.permissionTrigger.addEventListener(
+        "click",
+        this.allowOrientationPermissions()
+      );
       document.body.appendChild(this.permissionTrigger);
       // await this.attachDeviceOrientationHandler();
-    }catch(err){
+    } catch (err) {
       alert(err);
     }
-    
   }
-  
-  async allowOrientationPermissions(){
-    if (typeof DeviceOrientationEvent.requestPermission === "function") {
-      const permission = await DeviceOrientationEvent.requestPermission();
-      alert(permission)
-      if(permission == 'granted'){
-        window.addEventListener(
-          "deviceorientation",
-          this.deviceOrientationHandler.bind(this),
-          true
-        );
+
+  allowOrientationPermissions() {
+    return async (evt) => {
+      if (typeof DeviceOrientationEvent.requestPermission === "function") {
+        const permission = await DeviceOrientationEvent.requestPermission();
+        alert(permission);
+        if (permission == "granted") {
+          window.addEventListener(
+            "deviceorientation",
+            this.deviceOrientationHandler.bind(this),
+            true
+          );
+        } else {
+          throw new Error("no device orientation permissions!");
+        }
       } else {
-        throw new Error("no device orientation permissions!")
-      }
-    } else {
-      if (window.DeviceOrientationEvent) {
-        window.addEventListener(
-          "deviceorientation",
-          this.deviceOrientationHandler.bind(this),
-          true
-        );
-      } else {
-        alert("no device orientation support");
+        if (window.DeviceOrientationEvent) {
+          window.addEventListener(
+            "deviceorientation",
+            this.deviceOrientationHandler.bind(this),
+            true
+          );
+        } else {
+          alert("no device orientation support");
+        }
       }
     }
   }
