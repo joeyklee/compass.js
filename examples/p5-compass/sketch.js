03e4b6301d9ac675cc25c55b6ec807de1db02186
joeyklee <joeyklee@nyu.edu>
2020-04-23 13:38:03 -0400

    adds updates to lib using correct terminology

--- examples/p5-compass/sketch.js
+++ examples/p5-compass/sketch.js
@@ -1,23 +1,51 @@
-const compass = new Compass();
-compass.watchPosition();
+// let compass = new Compass();
+// compass.watchPosition();
 
-let pin;
+
+
+
+let compass;
+let headingAngle;
+let angleToNorth;
+let angleToLondon;
 function setup() {
   createCanvas(400, 400);
-  console.log(compass);
-  pin = createVector(0, 0);
-  // compass.update();
+  compass= new Compass(compassReady);
+
+  textAlign(CENTER);
+}
+
+function compassReady(){
+
+  headingAngle = compass.getHeading();
+  bearingToNorth = compass.getBearingToNorth();
+
+  const currentPosition = {lat: compass.position.coords.latitude, lng: compass.position.coords.longitude};
+  angleToLondon = compass.getBearingToDestination(currentPosition, {lat:51.507278, lng:-0.127821})
 }
 
 function draw() {
   background(220);
-  compass.getDesiredDirection({lat:30.815456, lng:-73.598510})
+  if(compass.position !== null){
+    compassReady();
+    text('heading angle', width*0.25, height*0.25)
+    drawPin(width*0.25, height*0.5, headingAngle);
 
+    text('angle to north', width*0.5, height*0.25)
+    drawPin(width*0.5, height*0.5, bearingToNorth);
+
+    text('angle to london', width*0.75, height*0.25)
+    drawPin(width*0.75, height*0.5, angleToLondon);
+  }
+  
+}
+
+function drawPin(x, y, pinAngle){
   push();
-  translate(width/2, height/2);
+  translate(x, y);
+  // 1. normalize for p5: p5 handles 0 degrees at +90deg from the web browser
   rotate(radians(-90) )
-  const pinAngle = (compass.deviceAngleFromNorth - compass.angleTowardsDesiredLocation)
-  console.log(pinAngle)
+  // 2. rotate accordingly
   rotate(radians( pinAngle) );
   circle(0,0, 10);
   line(0, 0, 50, 0);
--- src/Compass/Compass.js
+++ src/Compass/Compass.js
@@ -1,82 +1,184 @@
+// https://aviation.stackexchange.com/questions/8000/what-are-the-differences-between-bearing-vs-course-vs-direction-vs-heading-vs-tr
+
 class Compass {
   /**
    * @name Compass
    * @constructor
    */
-  constructor(){
-    this.deviceAngleFromNorth = 0;
+  constructor(callback = () => {}) {
+    this.bearing = 0;
+    this.heading = 0;
+
+    this.deviceAngleDelta = 0;
     this.position = null;
-    this.angleTowardsDesiredLocation = 0;
+    this.geolocationID = null;
+
     this.debug = false;
 
-    this.init();
+    this.ready = this.callCallback(this.init(), callback);
   }
 
   /**
    * Initializes the device orientation
    * @function
    */
-  init(){
+  async init() {
+    this.attachDeviceOrientationhandler();
+    await this.watchPosition();
+  }
+
+  /**
+   * @function
+   * Attaches the device orientaiton hanlder
+   */
+  attachDeviceOrientationhandler() {
     //   check for device orientation support
     if (window.DeviceOrientationEvent) {
       window.addEventListener(
-        'deviceorientation',
+        "deviceorientation",
         this.deviceOrientationHandler.bind(this),
         false
       );
     } else {
-      alert('no device orientation support');
+      alert("no device orientation support");
     }
   }
 
   /**
-   * handles changes created by the device orientation changes
+   * @function
+   * This is where my nose points - and seeing as my nose
+   * is attached to my head, this is where my head
+   * (and thus my machine) is pointing relative to North.
+   * NOTE: requires that this.position is set
+   */
+  getHeading(
+    origin = {
+      lat: this.position.coords.latitude,
+      lng: this.position.coords.longitude,
+    },
+    north = { lat: 90, lng: this.position.coords.longitude }
+  ) {
+    this.heading = 360 - this.getBearingToNorth(origin, north);
+    return this.heading;
+  }
+
+  /**
+   * @function
+   * This is the angle between the location of an object,
+   * machine or destination and my heading.
+   */
+  getBearing(origin, destination) {
+    return (
+      this.calculateAngle(
+        origin.lat,
+        origin.lng,
+        destination.lat,
+        destination.lng
+      ) *
+      (180 / Math.PI)
+    );
+  }
+
+  /**
+   * @function
+   * get the angle between your heading and north
+   * the default is true north vs. magnetic north
+   */
+  getBearingToNorth(
+    origin = {
+      lat: this.position.coords.latitude,
+      lng: this.position.coords.longitude,
+    },
+    north = { lat: 90, lng: this.position.coords.longitude }
+  ) {
+    return this.getBearingToDestination(origin, north)
+  }
+
+  /**
+   * 
+   * @param {*} origin 
+   * @param {*} destination 
+   */
+  getBearingToDestination(
+    origin = {
+      lat: this.position.coords.latitude,
+      lng: this.position.coords.longitude,
+    },
+    destination
+  ) {
+    const angleToDestination = this.getBearing(origin, destination);
+    return this.deviceAngleDelta + angleToDestination;
+  }
+
+  /**
+   * Handles changes created by the device orientation changes
+   * assumes that the phone is in a portrait mode, with the display up
+   * towards the sky as if you were holding an actual compass
    * @callback
    * @param {object} evt - the event object of the device orientation
    */
   deviceOrientationHandler(evt) {
     if (evt.webkitCompassHeading)
       //iphone
-      this.deviceAngleFromNorth = 360 - evt.webkitCompassHeading;
+      this.deviceAngleDelta = 360 - evt.webkitCompassHeading;
     else if (evt.alpha)
       //android
-      this.deviceAngleFromNorth = evt.alpha;
+      this.deviceAngleDelta = evt.alpha;
     else {
-      console.log('compass direction not found');
+      console.log("compass direction not found");
     }
 
-    this.deviceAngleFromNorth = Math.round(this.deviceAngleFromNorth);
+    this.deviceAngleDelta = Math.round(this.deviceAngleDelta);
   }
 
-   /**
+  /**
+   * @function
+   * @async
    * get the position of the user
    */
   getPosition() {
     return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(position => {
+      navigator.geolocation.getCurrentPosition((position) => {
         if (position) {
           resolve(position);
         } else {
-          reject('no position found');
+          reject("no position found");
         }
       });
     });
   }
 
-  watchPosition(){
-    return new Promise( (resolve, reject) => {
-      navigator.geolocation.watchPosition(position => {
+  /**
+   * @function
+   * @async
+   * watches the geolocation of the user
+   */
+  watchPosition() {
+    return new Promise((resolve, reject) => {
+      this.geolocationID = navigator.geolocation.watchPosition((position) => {
         if (position) {
           this.position = position;
-          // console.log(this.position.coords.latitude, this.position.coords.longitude)
+          console.log(
+            this.position.coords.latitude,
+            this.position.coords.longitude
+          );
           resolve(position);
         } else {
-          reject('no position found');
+          reject("no position found");
         }
       });
-    })
+    });
   }
 
+  /**
+   * Stops watching the user location
+   * @function
+   *
+   */
+  stopTracking() {
+    navigator.geolocation.clearWatch(this.geolocationID);
+    console.log("stopped tracking location");
+  }
 
   /**
    * Calculates the angle given a latitude and longitude position
@@ -87,26 +189,21 @@ class Compass {
    * @param {*} desiredLon - desired longitude
    */
   calculateAngle(userLat, userLon, desiredLat, desiredLon) {
-    return Math.atan2( desiredLon - userLon, desiredLat - userLat);
+    return Math.atan2(desiredLon - userLon, desiredLat - userLat);
   }
 
-  /**
-   * Get the desired location
-   * @async
-   * @function
-   */
-  getDesiredDirection(desiredLocation) {
-    // const position = await this.getPosition();
-    // console.log(position)
-    // calculate the angle to the most fit
-    this.angleTowardsDesiredLocation = this.calculateAngle(
-      this.position.coords.latitude,
-      this.position.coords.longitude,
-      desiredLocation.lat,
-      desiredLocation.lng
-    ) * (180/Math.PI);
-
-    // console.log("angle:", this.angleTowardsDesiredLocation, desiredLocation)
+  callCallback(promise, callback){
+    if (callback) {
+      promise
+        .then((result) => {
+          callback(undefined, result);
+          return result;
+        })
+        .catch((error) => {
+          callback(error);
+          return error;
+        });
+    }
+    return promise;
   }
-
 }
